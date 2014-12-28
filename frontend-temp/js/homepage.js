f7ac5f05929747290ac354e93f56d4039fd83eb5
dgli <facultyofmusic@gmail.com>
2014-12-27 20:22:33 -0500

    made basic swimming nodes as background test.  Future code please make sure whatever you do the background still works thank you

--- frontend-temp/js/homepage.js
+++ frontend-temp/js/homepage.js
@@ -1,115 +1,155 @@
 $(document).ready(function() {
 
-  function init() {
+  var ctx = null;
+  var objectList = [];
+  var printList = [];
+  var decc = 0.5;
 
-    var canvas = document.getElementById('background-animation');
-    console.log("Canvas width: " + canvas.width + " height: " + canvas.height);
+  function makeObject(ix, iy) {
+    console.log("Making at (" + ix + ", " + iy + ")");
 
+    var newObject = {
+      x: ix,
+      y: iy,
+      r: 10
+    };
 
-    clock();
-    setInterval(clock, 1000);
+    objectList.push(newObject);
+
+    return newObject;
   }
 
-  function clock() {
-    var now = new Date();
-    var ctx = document.getElementById('background-animation').getContext(
-      '2d');
+  function makePrint(ix, iy, id) {
+    var newPrint = {
+      x: ix,
+      y: iy,
+      d: id,
+      tc: 0,
+      speed: 5,
+      rotationSpeed: 0.5
+    };
+
+    printList.push(newPrint);
+  }
+
+
+  function init() {
+    ctx = document.getElementById('background-animation').getContext('2d');
+
+
+    makePrint(400, 400, 1);
+    makePrint(400, 400, 1);
+    makePrint(400, 400, 1);
+    makePrint(400, 400, 1);
+    makePrint(400, 400, 1);
+    makePrint(400, 400, 1);
+    makePrint(400, 400, 1);
+
+    renderLoop();
+  }
+
+
+  function renderLoop() {
+    update();
+    render();
+    requestAnimationFrame(renderLoop);
+  }
+
+
+
+  function update() {
+
+    // update objects
+    printList.forEach(function(ob) {
+
+      // calculate new position
+      ob.x += ob.speed * Math.sin(ob.d);
+      ob.y += ob.speed * Math.cos(ob.d);
+
+      // calculate movement direction
+      ob.d += ob.rotationSpeed * (Math.random() - 0.5);
+
+
+      // calculate potential object spawning
+      ob.tc++;
+
+      if (ob.tc > 5) {
+        ob.tc = 0;
+        makeObject(ob.x, ob.y);
+      }
+
+      // position correction
+      var width = window.innerWidth;
+      var height = window.innerHeight;
+
+      if (ob.x < 0) {
+        ob.x = width;
+      }
+
+      if (ob.y < 0) {
+        ob.y = height;
+      }
+
+
+      if (ob.x > width) {
+        ob.x = 0;
+      }
+
+
+      if (ob.y > height) {
+        ob.y = 0;
+      }
+
+
+    });
+
+
+    // update objects
+    objectList.forEach(function(ob) {
+
+      if (ob.r > decc) {
+        ob.r -= decc;
+      } else {
+        ob.r = 0;
+      }
+
+    });
+
+    for (var i = 0; i < objectList.length; ++i) {
+      if (objectList[i].r == 0) {
+        objectList.splice(i--, 1);
+        console.log("object is dead. removing");
+      }
+    }
+  }
+
+
+  function render() {
 
     ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
 
-    ctx.save();
-    ctx.clearRect(0, 0, 150, 150);
-    ctx.translate(75, 75);
-    ctx.scale(0.4, 0.4);
-    ctx.rotate(-Math.PI / 2);
-    ctx.strokeStyle = "black";
-    ctx.fillStyle = "white";
-    ctx.lineWidth = 8;
-    ctx.lineCap = "round";
+    printList.forEach(function(ob) {
 
-    // Hour marks
-    ctx.save();
-    for (var i = 0; i < 12; i++) {
       ctx.beginPath();
-      ctx.rotate(Math.PI / 6);
-      ctx.moveTo(100, 0);
-      ctx.lineTo(120, 0);
+      ctx.lineWidth = 14;
+      ctx.strokeStyle = '#AAAAAA';
+      ctx.arc(ob.x, ob.y, 5, 0, Math.PI * 2, true);
       ctx.stroke();
-    }
-    ctx.restore();
 
-    // Minute marks
-    ctx.save();
-    ctx.lineWidth = 5;
-    for (i = 0; i < 60; i++) {
-      if (i % 5 != 0) {
-        ctx.beginPath();
-        ctx.moveTo(117, 0);
-        ctx.lineTo(120, 0);
-        ctx.stroke();
-      }
-      ctx.rotate(Math.PI / 30);
-    }
-    ctx.restore();
+    });
 
-    var sec = now.getSeconds();
-    var min = now.getMinutes();
-    var hr = now.getHours();
-    hr = hr >= 12 ? hr - 12 : hr;
+    objectList.forEach(function(ob) {
 
-    ctx.fillStyle = "black";
+      ctx.beginPath();
+      ctx.lineWidth = 14;
+      ctx.strokeStyle = '#325FA2';
+      ctx.arc(ob.x, ob.y, ob.r, 0, Math.PI * 2, true);
+      ctx.stroke();
 
-    // write Hours
-    ctx.save();
-    ctx.rotate(hr * (Math.PI / 6) + (Math.PI / 360) * min + (Math.PI /
-        21600) *
-      sec)
-    ctx.lineWidth = 14;
-    ctx.beginPath();
-    ctx.moveTo(-20, 0);
-    ctx.lineTo(80, 0);
-    ctx.stroke();
-    ctx.restore();
+    });
 
-    // write Minutes
-    ctx.save();
-    ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec)
-    ctx.lineWidth = 10;
-    ctx.beginPath();
-    ctx.moveTo(-28, 0);
-    ctx.lineTo(112, 0);
-    ctx.stroke();
-    ctx.restore();
 
-    // Write seconds
-    ctx.save();
-    ctx.rotate(sec * Math.PI / 30);
-    ctx.strokeStyle = "#D40000";
-    ctx.fillStyle = "#D40000";
-    ctx.lineWidth = 6;
-    ctx.beginPath();
-    ctx.moveTo(-30, 0);
-    ctx.lineTo(83, 0);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(0, 0, 10, 0, Math.PI * 2, true);
-    ctx.fill();
-    ctx.beginPath();
-    ctx.arc(95, 0, 10, 0, Math.PI * 2, true);
-    ctx.stroke();
-    ctx.fillStyle = "rgba(0,0,0,0)";
-    ctx.arc(0, 0, 3, 0, Math.PI * 2, true);
-    ctx.fill();
-    ctx.restore();
-
-    ctx.beginPath();
-    ctx.lineWidth = 14;
-    ctx.strokeStyle = '#325FA2';
-    ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
-    ctx.stroke();
-
-    ctx.restore();
   }
 
   init();
