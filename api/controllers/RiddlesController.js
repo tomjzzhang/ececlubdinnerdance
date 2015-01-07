51f63cd3ad81fb5f2b0586e90042897de40fcbd4
SirTroll <zcchew2011@gmail.com>
2015-01-06 22:07:39 -0500

    riddle page 25% done

---
+++ api/controllers/RiddlesController.js
@@ -0,0 +1,28 @@
+/**
+ * RiddlesController
+ *
+ * @description :: Server-side logic for managing riddles
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+
+
+	'new_riddles' : function (req,res){
+		res.view();
+	},
+
+
+//create: function (req, res, next){
+
+	/* Create a User with the params sent from
+			the sign-up form -> new_riddles.ejs
+	*/
+	//Riddles.create(req.params.all(), function )
+
+
+
+
+
+};
---
+++ api/models/Riddles.js
@@ -0,0 +1,27 @@
+/**
+* Riddles.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+    email: {
+      type: 'string',
+      email: true,
+      required: true,
+      unique: true
+
+    },
+
+    answer1: {
+      type: 'string',
+      required: true,
+    }
+
+
+  }
+};
