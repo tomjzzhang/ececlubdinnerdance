b84364fcd11bc5aeae1f665412c47f4607cb1b78
Hassan Rezvani <hassan.rezvani10@gmail.com>
2015-01-08 15:53:05 -0500

    Modified the Ticketcontroller

--- api/controllers/TicketController.js
+++ api/controllers/TicketController.js
@@ -7,5 +7,21 @@
 
 module.exports = {
 	
+	'new': function(req, res) {
+		res.view();
+	},
+
+	create: function(req, res, next) {
+		Ticekt.create(req.params.all(), function(err, ticket) {
+			if (err) {
+				console.log(err);
+				req.session.flash = {
+					err: err.validationError
+				}
+
+				return res.redirect('/ticket/new');
+			}
+		});
+	}
 };
 
