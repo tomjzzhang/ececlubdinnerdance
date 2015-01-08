4dfb5296b7fa52ab53da52305ab2220e935c05e7
MikeSong95 <mike.song@mail.utoronto.ca>
2015-01-08 18:19:18 -0500

    Tables page first commit

---
+++ api/controllers/TablesController.js
@@ -0,0 +1,18 @@
+/**
+ * TablesController
+ *
+ * @description :: Server-side logic for managing tables
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	'index': function(req, res) {
+		res.view();
+	}
+
+	var btn_add1 = document.getElementById("add_table1")
+
+	btn_add1.onclick = color() {
+		style.backgroundColor = '#FFFF00';
+	};
+};
