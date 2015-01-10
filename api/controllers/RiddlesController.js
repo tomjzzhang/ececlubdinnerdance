a33e0e70e48af21584a40849b11a114fbf5884de
SirTroll <zcchew2011@gmail.com>
2015-01-10 17:33:25 -0500

    RegExp for UTmail field for raffle

--- api/controllers/RiddlesController.js
+++ api/controllers/RiddlesController.js
@@ -17,9 +17,26 @@ module.exports = {
 		res.view();
 	},
 
-//.+@mail\.utoronto\.ca$ ->regex to check
+
 	create: function (req, res, next){
-//hard code riddles here
+	//check if user entered UTmail
+	//.+@mail\.utoronto\.ca$ ->regexp to check
+	var UTmail_pattern = new RegExp(".+@mail\.utoronto\.ca$");
+
+
+
+	if(!UTmail_pattern.test(req.param('email'))){
+		var emailError = [{name: 'UTmail is required', message: 'Email entered is not a UTmail. UTmail is required for the raffle! eg: xxx@mail.utoronto.ca'}]
+		req.session.flash = {
+			err: emailError
+		}
+		console.log("email is not a UTmail");
+		res.redirect('/riddles/new_riddles');
+		return;
+
+	}
+
+		//hard code riddles here
 	if(req.param('ans_Riddle_1') != "qwerty"){
 		var riddleError_1 = [{name: 'AnswerIsWrong', message: 'The answer to this riddle is incorrect.'}]
 		req.session.flash = {
