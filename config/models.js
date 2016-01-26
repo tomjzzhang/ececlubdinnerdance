45a9ac83a8629cd14b6713e00834323ddd53778b
zcchew1202 <zcchew2011@gmail.com>
2016-01-26 14:57:55 -0500

    trying to deploy 2nd try

--- config/models.js
+++ config/models.js
@@ -18,7 +18,7 @@ module.exports.models = {
    *                                                                          *
    ***************************************************************************/
   //connection: 'localDiskDb', //use this if for development
-  connection: 'mongodbServer',
+  // connection: 'mongodbServer',
 
   /***************************************************************************
    *                                                                          *
