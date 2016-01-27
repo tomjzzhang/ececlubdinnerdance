4d4817636084bd0c9d0befc790d6c349f0d3a65a
zcchew1202 <zcchew2011@gmail.com>
2016-01-26 19:37:53 -0500

    trying to deploy 19th try, installed grunt-cli in sails

--- post_install.sh
+++ post_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./node_modules/bower/bin/bower install
-./ececlubdinnerdance/node_modules/sails/node_modules/grunt-cli/bin/grunt
+./app/node_modules/sails/node_modules/grunt-cli/bin/grunt
